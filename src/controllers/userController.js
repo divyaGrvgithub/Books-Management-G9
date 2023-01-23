dc55bef81d9094eb981183cacd0da20fa7727cbf
divyaGrvgithub <divyamalatiwarimtech@gmail.com>
2023-01-23 12:50:46 +0530

    user created

---
+++ src/controllers/userController.js
@@ -0,0 +1,14 @@
+const userModel = require("../Models/userModel")
+const jwt = require("jsonwebtoken")
+
+// <----------------------------Create User----------------------------->
+
+exports.createUser = async () => {
+    try {
+        let data = req.body
+        const savedData = await userModel.create(data)
+        res.status(201).send({ status: true, msg: savedData })
+    } catch (error) {
+        res.status(500).send({ status: false, error:error.message })
+    }
+}

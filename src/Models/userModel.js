fdad2f8b297ec40680bb6e0b7c890b05ef5b57a6
divyaGrvgithub <divyamalatiwarimtech@gmail.com>
2023-01-23 12:29:39 +0530

    usermodel

---
+++ src/Models/userModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        reuired:true,
+        enum:["Mr","Mrs","Miss"]
+    },
+    name:{
+        type:String,
+        required:true
+    },
+    phone:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    address:{
+        street: String,
+        city: String,
+        pincode: String
+    }},
+    {timestamps:true})
+
+    module.exports=mongoose.model("User",userSchema)
